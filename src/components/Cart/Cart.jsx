7153c65fb733dd718c5892fb17fd7f707dbc594e
Felipe Arce <arceprogramando@gmail.com>
2023-03-23 15:52:43 -0300

    Actualizacion Prefinal

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,25 +1,35 @@
+import './cart.css'
+
 import React, { useContext } from 'react';
 import { CartContext } from '../../contexts/ShoppingCartProvider';
 
 const Cart = () => {
     const [cart, setCart] = useContext(CartContext);
 
+    const removeItemFromCart = (id) => {
+        const updatedCart = cart.filter((item) => item.id !== id);
+        setCart(updatedCart);
+    };
     return (
-        <div>
-            <h2>Carrito de compras</h2>
-            <ul>
-                {cart.map(item => (
-                    <li key={item.id}>
-                        <img src={item.url} alt={item.nombre} />
-                        <div>
-                            <h3>{item.nombre}</h3>
-                            <p>Cantidad: {item.quantity}</p>
-                            <p>Precio unitario: ${item.precio}</p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        < >
+            <div className='main container'>
+
+                <h2>Carrito de compras</h2>
+                <ul>
+                    {cart.map(item => (
+                        <li key={item.id}>
+                            <img src={item.url} alt={item.nombre} />
+                            <div>
+                                <h3>{item.nombre}</h3>
+                                <p>Cantidad: {item.quantity}</p>
+                                <p>Precio unitario: ${item.precio}</p>
+                            </div>
+                            <button onClick={() => removeItemFromCart(item.id)}>Eliminar</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 };
 
--- src/components/NavBar/CardWidget/CartWidget.jsx
+++ src/components/NavBar/CardWidget/CartWidget.jsx
@@ -4,9 +4,7 @@ import { CartContext } from '../../../contexts/ShoppingCartProvider';
 
 const CartWidget = () => {
   const [cart, setCart] = useContext(CartContext);
-  const quantity = cart.reduce((acc, curr) => {
-    return acc + curr.quantity;
-  }, 0)
+  const quantity = cart.length;
   return (
     <div>
       <img src='/cart.svg'></img>
@@ -16,3 +14,7 @@ const CartWidget = () => {
 }
 
 export default CartWidget;
+
+
+
+
